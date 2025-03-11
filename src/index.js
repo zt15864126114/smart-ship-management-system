debb591bbe4ebe8a193f53d5ceaa2565cd88ea47
zhaoteng <1813831021@qq.com>
2025-03-11 16:57:39 +0800

    修复报错

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -12,8 +12,8 @@ const basename = process.env.NODE_ENV === 'production'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter basename={basename}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 ); 
