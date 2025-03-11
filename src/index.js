46d8d67c7842d5341e40293827bc3722ba7c468b
zhaoteng <1813831021@qq.com>
2025-03-11 17:08:47 +0800

    添加图标生成和修复资源路径

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-// 根据环境判断是否需要添加basename
-const basename = process.env.NODE_ENV === 'production' 
-  ? '/smart-ship-management-system'
-  : '';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={basename}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
