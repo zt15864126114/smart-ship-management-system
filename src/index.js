f09ccc8b9a7f75eba4df3d839985313e71dd6c1b
zhaoteng <1813831021@qq.com>
2025-03-11 16:50:15 +0800

    修复报错

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+// 根据环境判断是否需要添加basename
+const basename = process.env.NODE_ENV === 'production' 
+  ? '/smart-ship-management-system'
+  : '';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
